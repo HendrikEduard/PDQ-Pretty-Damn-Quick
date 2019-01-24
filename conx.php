9c0b9f0827e540c16568f8ab60f42587b67c23d3
Hendrik Eduard Kuiper <16320682+HendrikEduard@users.noreply.github.com>
2019-01-24 11:57:50 +0100

    Update conx.php

--- conx.php
+++ conx.php
@@ -1,2 +1,2 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '1984password', 'bbf1ac0aa0c5053148bb8c0') or die($mysqli_error($mysqli));
+$mysqli = new mysqli('localhost', 'never_use_root', 'never_use_password', 'bbf1ac0aa0c5053148bb8c0') or die($mysqli_error($mysqli));
