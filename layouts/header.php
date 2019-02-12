aedaa788e0cbc87d5d05a09469e8765356ab9e1f
Hendrik Eduard Kuiper <16320682+HendrikEduard@users.noreply.github.com>
2019-02-12 17:08:40 +0100

    Update

--- layouts/header.php
+++ layouts/header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$title?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/app.css">
+    <title><PDQ (Pretty 'n' Damn Quick)></title>
   </head>
+ <php require_once 'nav.php'
