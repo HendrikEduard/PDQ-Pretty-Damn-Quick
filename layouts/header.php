e3082562efab1ce5710ebc1ea2f427e8223914dc
Hendrik Eduard Kuiper <16320682+HendrikEduard@users.noreply.github.com>
2019-05-04 15:50:56 +0200

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><PDQ (Pretty 'n' Damn Quick)></title>
+    <title><PDQ (Pretty Damn Quick)></title>
   </head>
  <php require_once 'nav.php'
